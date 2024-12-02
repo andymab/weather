70fa9e4f8b1b5aaeef841d9afd6939fde8e021e1
Andy hu <650517@355042.ru>
2024-12-02 21:16:52 +0300

    fix locations

--- src/components/WeatherForm.vue
+++ src/components/WeatherForm.vue
@@ -35,6 +35,8 @@
 import axios from 'axios';
 import WeatherChart from './WeatherChart.vue';
 import WeatherDataTable from './WeatherDataTable.vue';
+import locations from '../config/locations';
+
 
 export default {
     components: {
@@ -49,23 +51,7 @@ export default {
             longitude: null,
             weatherData: [],
             comparisonWeatherData: null,
-            locations: [
-                { title: 'Адлер', value: { label:'Адлер',lat: 43.39646, lon: 39.97040 } },
-                { title: 'Сочи', value: { label:'Сочи', lat: 43.57782, lon: 39.72106 } },
-                { title: 'Архыз', value: { label:'Архыз', lat: 43.52359, lon: 41.23145 } },
-                { title: 'Домбай', value: { label:'Домбай', lat: 43.29091, lon: 41.62691 } },
-                { title: 'Судак', value: { label:'Судак',  lat: 44.83977, lon: 34.9157 } },
-                { title: 'Новый Свет', value: { label:'Новый Свет',  lat: 44.8317, lon: 34.9157 } },
-                { title: 'Ялта', value: { label:'Ялта',  lat: 44.49444, lon: 34.15374 } },
-                { title: 'Ласпи', value: {  label:'Ласпи', lat: 44.41270, lon: 33.71343 } },
-                { title: 'Форос', value: {  label:'Форос', lat: 44.39223, lon: 33.79233 } },
-                { title: 'Севастополь', value: {  label:'Севастополь', lat: 44.63154, lon: 33.51329 } },
-                { title: 'Алушта', value: {  label:'Алушта', lat: 44.67144, lon: 34.41246 } },
-                { title: 'Ставрополь-дом', value: {  label:'Ставрополь-дом', lat: 44.99941, lon: 41.92428 } },
-                { title: 'Ставрополь-дача', value: {  label:'Ставрополь-дача', lat: 45.00224, lon: 41.96936 } },
-                { title: 'Ставрополь-техникум', value: {  label:'Ставрополь-техникум', lat: 45.02932, lon: 41.97437 } },
-                { title: 'Ставрополь-Аврора', value: {  label:'Ставрополь-Аврора', lat: 45.00224, lon: 42.00529 } },
-            ],
+            locations,
         };
     },
     methods: {
